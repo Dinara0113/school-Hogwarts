c4b4c71c154d01d19d3a8a3c98bf0d48e4c57394
Dinara0113 <dinarkatttb16@gmail.com>
2025-03-22 16:41:12 +0500

    hw-4.2

--- src/main/resources/sql/scripts421.sql
+++ src/main/resources/sql/scripts421.sql
@@ -30,7 +30,6 @@ CREATE TABLE IF NOT EXISTS Person_Car (
     car_id INT REFERENCES Car(id) ON DELETE CASCADE,
     PRIMARY KEY (person_id, car_id)
 );
-
 SELECT s.name AS student_name, s.age, f.name AS faculty_name
 FROM Student s
 JOIN Faculty f ON s.faculty_id = f.id;
